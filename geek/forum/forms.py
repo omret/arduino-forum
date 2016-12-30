8163d120f292fc0b938732286661560d24a1f0c8
terry.li <terry.li@dorry.io>
2016-12-30 17:41:39 +0800

    add tinymce

--- geek/forum/forms.py
+++ geek/forum/forms.py
@@ -4,8 +4,7 @@ class NewTopicForm(forms.Form):
     ##-------the form of title------------
     title = forms.CharField(required=True,
                             widget=forms.TextInput(
-                                attrs={"id": "titleform",
-                                       "class": "form-control"}
+                                attrs={"id": "titleform"}
                             ))
 
     content = forms.CharField(required=True,
