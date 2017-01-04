909bcc508d94684330087ea2a8f1f74cb791fc04
terry.li <terry.li@dorry.io>
2017-01-04 18:23:24 +0800

    add form item

--- geek/forum/urls.py
+++ geek/forum/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^forum/$', views.forum,name='forum'),
+    url(r'^$', views.forum,name='forum'),
     url(r'^forum/new$', views.forum_new)
 ]
