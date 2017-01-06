93a1d1970db7133d737f51a06b662b78af1b03a4
terry.li <terry.li@dorry.io>
2017-01-06 19:39:31 +0800

    style topic list item container

--- geek/forum/urls.py
+++ geek/forum/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.forum,name='forum'),
-    url(r'^forum/new$', views.forum_new)
+    url(r'^forum/new$', views.forum_new),
+    url(r'^forum/topic(\d+)$',views.topic_index),
 ]
--- geek/forum/views.py
+++ geek/forum/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from omretuser.models import User
 from .forms import NewTopicForm
+from .models import Topic,Comment
 
 # Create your views here.
 def forum(request):
     user = __getUserFromSession(request)
     return render(request,'forum/forum.html',{"user":user})
+
+def topic_index(request,index):
+    user = __getUserFromSession(request)
+    try:
+        topic = Topic.objects.get(id=index)
+    except Exception as e:
+        return HttpResponseRedirect('/')
+        print(e)
+    try:
+        topic_comment = Comment.objects.get(topic=topic)
+    except Exception as e:
+        topic_commnet = None
+
+    return render(request,'forum/topic_index.html',{})
 
 def forum_new(request):
     user = __getUserFromSession(request)
